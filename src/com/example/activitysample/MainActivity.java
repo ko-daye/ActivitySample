db2730b17c40ecfa7aaa3da671a49efe82582c69
ko-daye <kodaye@gmail.com>
2013-10-10 23:47:28 +0900

    add sample2

--- src/com/example/activitysample/MainActivity.java
+++ src/com/example/activitysample/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.activitysample;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +17,54 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button but = (Button) findViewById(R.id.button1);
+		but.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, SubActivity.class));
+			}
+		});
+		setImage();
 	}
+
+	int[] images = new int[] { R.drawable.ic_launcher, R.drawable.droid_1,
+			R.drawable.droid2 };
+	int imageResource = -1;
+	ImageView mImageView = null;
+
+	private void setImage() {
+
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+		Button mbutton = (Button) findViewById(R.id.button2);
+		mbutton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Random rand = new Random();
+				imageResource = images[Math.abs(rand.nextInt()) % images.length];
+				mImageView.setImageResource(imageResource);
+			}
+		});
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+//		if (imageResource != -1) {
+//			outState.putInt("imageResource", imageResource);
+//		}
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+
+//		if (savedInstanceState != null) {
+//			imageResource = savedInstanceState.getInt("imageResource");
+//			mImageView.setImageResource(imageResource);
+//		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
